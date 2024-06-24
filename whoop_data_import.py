ffaa13f6cc33c0e46d7a3f9e5e31a96ddab35f7c
Richy <richardvarthurs@gmail.com>
2024-06-24 17:08:12 -0400

    first notebook

--- whoop_data_import.py
+++ whoop_data_import.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
 # Read the CSV file
-file_path = "workouts.csv"
-df = pd.read_csv(file_path)
+file_path = "sleeps.csv"
+sleep_data = pd.read_csv(file_path)
 
-print(df.head())
+
+# Check for missing values
+print(sleep_data.isnull().sum())
+
+# Fill or drop missing values if necessary
+sleep_data = sleep_data.dropna()
+
+# Convert date columns to datetime objects if needed
+sleep_data['date'] = pd.to_datetime(sleep_data['date'])
